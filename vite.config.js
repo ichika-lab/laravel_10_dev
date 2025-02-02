18e75f3ba4ebd1c161fd7be6f3ad25fec916436f
ichika_mac <toka.kumin.1057@gmail.com>
2025-02-02 12:39:21 +0900

    vue?

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -11,9 +12,12 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+        vue(),
     ],
     server: {
         host: true,
+        // ost:'xxx.xxx.xxx.xxx', // ipを記載する
+        // port: 5731,
         hmr: {
             host: 'localhost',
         },
